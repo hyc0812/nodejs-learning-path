a957d58cefe80efe74441fc19219ffb90b2cd21c
yongchang he <hyc0812@163.com>
2022-05-05 11:17:18 -0600

    update

---
+++ 3_modules&require/modules.js
@@ -0,0 +1,15 @@
+const { people, ages } = require('./people');
+
+// Ouput: {}
+// console.log(xyz);
+
+// Output: undefined
+// console.log(xyz.people);
+
+
+console.log(people, ages);
+
+// import os from 'os';
+const os = require('os');
+
+console.log(os.arch(), os.platform(), os.homedir());
---
+++ 3_modules&require/people.js
@@ -0,0 +1,7 @@
+const people = ['yoshi','ryu', 'chun-li', 'mario', 'luigi'];
+const ages = [20, 30, 40, 50, 60];
+// console.log(people);
+
+module.exports = {
+    people, ages
+};
