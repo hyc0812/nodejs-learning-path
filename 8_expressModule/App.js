5ecc50c24a3f82252168e85ddd0bcc59e4e314eb
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-05-05 22:59:13 -0600

    update

--- 8_expressModule/App.js
+++ 8_expressModule/App.js
@@ -9,5 +9,17 @@ app.listen(3000);
 
 
 app.get('/', (req, res) => {
-    res.send('<p> Home page Hello world using express and nodemon </p>');
+    // res.send('<p> Home page Hello world using express and nodemon </p>');
+    // root helps to set the root path, so that we can use a relative path
+    res.sendFile('./views/index.html', { root: __dirname});
+})
+
+app.get('/about', (req, res) => {
+    // res.send('<p> This is the about page </p>');
+    res.sendFile('./views/about.html', { root: __dirname});
+})
+
+// redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
 })
