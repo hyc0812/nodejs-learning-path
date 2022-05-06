153a247b9e2568aa13908f7686757cbbad8479ce
yongchang he <hyc0812@163.com>
2022-05-06 14:20:23 -0600

    udpate

--- 9_viewEngines/App.js
+++ 9_viewEngines/App.js
@@ -1,34 +1,49 @@
 // People call this file App.js using express module
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
 // MongoDB URI
-const mongoURI = 'mongodb+srv://yongchanghe:abcdABCD12345@node-for-yong.yojr5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// A reminder that use own name, password, and database name
+const mongoURI = 'mongodb+srv://yongchanghe:abcdABCD12345@node-for-yong.yojr5.mongodb.net/yong-project-001?retryWrites=true&w=majority';
 
+// connect to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch(err => console.log(err));
 // register view engine, important
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
-
-// listen for requests
-app.listen(3000);
 
 // use the middleware from third party
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'New Blog',
+        snippet: 'about my new blog',
+        body: 'New Blog Body'
+    });
+    blog.save()
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
 // make my own middleware
 // app.use((req, res, next) => {
 //     console.log('new request made');
 //     console.log('host:', req.hostname);
-//     console.log('path:', req.path);
-//     console.log('method:', req.method);
-//     console.log('-------------------------------');
-//     next();
-// });
+
 
 app.get('/', (req, res) => {
     const blogs = [
