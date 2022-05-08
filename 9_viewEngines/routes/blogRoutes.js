990060d0efdef25f497f9ecbc64b1fe306556776
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-05-08 00:38:36 -0600

    update

--- 9_viewEngines/routes/blogRoutes.js
+++ 9_viewEngines/routes/blogRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 // render the blogs page to the index.ejs
-router.get('/blogs', (req, res) => {
+router.get('/', (req, res) => {
     Blog.find().sort({ createdAt: 'desc' }) // or using { createdAt: -1 }
         .then(result => {
             res.render('index', { title: 'All Blogs', blogs: result });
@@ -16,7 +16,7 @@ router.get('/blogs', (req, res) => {
 });
 
 // submit a new blog using post method to the database
-router.post('/blogs', (req, res) => {
+router.post('/', (req, res) => {
     const blog = new Blog(req.body);
     blog.save()
         .then(result => {
@@ -27,11 +27,11 @@ router.post('/blogs', (req, res) => {
         })
 });
 
-router.get('/blogs/create', (req, res) => {
+router.get('/create', (req, res) => {
     res.render('create', { title: 'Create a new blog' });
 })
 
-router.get('/blogs/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const id = req.params.id;
     Blog.findById(id)
         .then(result => {
@@ -42,7 +42,7 @@ router.get('/blogs/:id', (req, res) => {
         })
 });
 
-router.delete('/blogs/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     const id = req.params.id;
     Blog.findByIdAndDelete(id)
     .then(result => {
