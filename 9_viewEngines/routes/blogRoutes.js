51120fc4455747227ec0d9715c2f176e909fd94e
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-05-08 22:37:10 -0600

    update MVC

--- 9_viewEngines/routes/blogRoutes.js
+++ 9_viewEngines/routes/blogRoutes.js
@@ -1,57 +1,22 @@
 const express = require('express');
 const Blog = require('../models/blog');
 
+const blogController = require('../controllers/blogController');
+
 const router = express.Router();
 
 
 // render the blogs page to the index.ejs
-router.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: 'desc' }) // or using { createdAt: -1 }
-        .then(result => {
-            res.render('index', { title: 'All Blogs', blogs: result });
-        })
-        .catch(err => {
-            console.log(err);
-        })
-});
+router.get('/', blogController.blog_index);
 
 // submit a new blog using post method to the database
-router.post('/', (req, res) => {
-    const blog = new Blog(req.body);
-    blog.save()
-        .then(result => {
-            res.redirect('/blogs');
-        })
-        .catch(err => {
-            console.log(err);
-        })
-});
+router.post('/', blogController.blog_create_post);
 
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-})
+router.get('/create', blogController.blog_create_get);
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: 'Blog Details' });
-        })
-        .catch(err => {
-            console.log(err);
-        })
-});
+router.get('/:id', blogController.blog_details);
 
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-    .then(result => {
-        res.json({ redirect: '/blogs' })
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
+router.delete('/:id', blogController.blog_delete);
 
 
 
