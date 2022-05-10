4e5b42a077a105d8984e2945ce257f8d3efb3a2a
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-05-09 19:23:05 -0600

    Restructure MVC

--- 9_viewEngines/routes/blogRoutes.js
+++ 9_viewEngines/routes/blogRoutes.js
@@ -1,23 +1,14 @@
 const express = require('express');
-const Blog = require('../models/blog');
-
 const blogController = require('../controllers/blogController');
-
 const router = express.Router();
-
 
 // render the blogs page to the index.ejs
 router.get('/', blogController.blog_index);
-
 // submit a new blog using post method to the database
 router.post('/', blogController.blog_create_post);
-
 router.get('/create', blogController.blog_create_get);
-
 router.get('/:id', blogController.blog_details);
-
 router.delete('/:id', blogController.blog_delete);
-
 
 
 module.exports = router;
